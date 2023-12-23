f79b9d2399d72b31c9702738ff8eeae0e308c331
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-12-23 19:02:04 +0300

    Add login page UI and assets

--- Trackly.APP/trackly_app/lib/src/pages/login_page.dart
+++ Trackly.APP/trackly_app/lib/src/pages/login_page.dart
@@ -5,6 +5,8 @@ import 'package:trackly_app/src/bloc/auth/auth_bloc/auth_bloc.dart';
 import 'package:trackly_app/src/bloc/auth/auth_bloc/auth_event.dart';
 import 'package:trackly_app/src/bloc/auth/auth_bloc/auth_state.dart';
 import 'package:trackly_app/src/bloc/auth/authentication_bloc/authentication_bloc.dart';
+import 'package:trackly_app/src/utils/all_constants_imports.dart';
+import 'package:trackly_app/src/utils/app_resources.dart';
 import 'package:trackly_app/src/utils/routes.dart';
 
 class MyLoginPage extends StatefulWidget {
@@ -36,22 +38,67 @@ class _MyLoginPageState extends State<MyLoginPage> {
         }
       },
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                  onPressed: () async {
-                    context.read<AuthBloc>().add(
-                          const Login(),
-                        );
-                  },
-                  child: const Text("Log in"))
-            ],
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 7.0, right: 7.0),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  //Login picture
+                  Image.asset(
+                    AppAssets.loginpic,
+                    width: MediaQuery.of(context).size.width * 1,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Text(
+                    'Trackly',
+                    style: AppResources().appStyles.textStyles.headineH4,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                  ),
+                  //Centered paragraph
+                  Text(
+                    'Revolutionizing technical fault management for streamlined operations. Log in to create work tickets, manage assets, and expedite issue resolutions for enhanced productivity.',
+                    style: AppResources().appStyles.textStyles.componentsFilter,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: OutlinedButton(
+                      onPressed: () async {
+                        context.read<AuthBloc>().add(
+                              const Login(),
+                            );
+                      },
+                      style: AppResources()
+                          .buttonStyles
+                          .buttonStyle(backgroundColor: Colors.black),
+                      child: Text(
+                        "LOGIN",
+                        style: AppResources()
+                            .appStyles
+                            .textStyles
+                            .componentsButtonDefault
+                            .copyWith(
+                              color: Colors.white,
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- Trackly.APP/trackly_app/lib/src/utils/app_assets.dart
+++ Trackly.APP/trackly_app/lib/src/utils/app_assets.dart
@@ -2,4 +2,5 @@ class AppAssets {
   static const String homeIcon = "assets/images/homeIcon.png";
   static const String profileIcon = "assets/images/profileIcon.png";
   static const String scanIcon = "assets/images/scanIcon.png";
+  static const String loginpic = "assets/images/loginPic.png";
 }
