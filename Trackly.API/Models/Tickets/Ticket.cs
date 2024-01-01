795f3b84ab6e00cb24fd8d17768902971b8de3c3
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-01 21:22:14 +0300

    Add Ticket and WorkItem classes to Models/Tickets directory

---
+++ Trackly.API/Models/Tickets/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using TracklyApi.Models.Assets;
+using static TracklyApi.Helpers.EnumHelper;
+
+namespace TracklyApi.Models.Tickets
+{
+    public class Ticket
+    {
+        public Guid TicketId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TicketStatus Status { get; set; }
+        public Priority Priority { get; set; }
+        public TicketCategory Category { get; set; }
+        public Guid? AssignedUserID { get; set; }
+        public Guid AssetID { get; set; }
+
+        //Time created for the ticket
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation properties
+        public virtual Asset Asset { get; set; }
+    }
+}
