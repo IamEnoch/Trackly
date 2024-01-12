a37fe3e3b41875c6698be95da601cdf4ebd0bb49
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-13 00:04:52 +0300

    Add new pages and models

---
+++ Trackly.APP/trackly_app/lib/src/data/models/status_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StatusColor {
+  Color textColor;
+  Color backgroundColor;
+
+  StatusColor({
+    this.textColor = const Color(0xffFFFFFF),
+    required this.backgroundColor,
+  });
+}
