f117fb0ed647c61f624134b952e73a67f6eac306
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-18 22:07:22 +0300

    Add UserDto class

---
+++ Trackly.API/DTOs/UserDto.cs
@@ -0,0 +1,20 @@
+﻿namespace TracklyApi.DTOs
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string UserId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public UserDto(string email, string username, string userId, DateTime? createdAt, DateTime? updatedAt)
+        {
+            this.Email = email;
+            this.Username = username;
+            this.UserId = userId;
+            this.CreatedAt = createdAt;
+            this.UpdatedAt = updatedAt;
+        }
+    }
+}
