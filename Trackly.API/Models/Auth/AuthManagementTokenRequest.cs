d932a3ca2bcae64e0e64beb4cdf70ffda8b400db
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-18 22:06:55 +0300

    Add AuthManagementTokenRequest, AuthManagementTokenResponse, AuthUser, AuthUserIdentity, and UserAuthManagementTokenResponse classes

---
+++ Trackly.API/Models/Auth/AuthManagementTokenRequest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace TracklyApi.Models.Auth;
+
+public class AuthManagementTokenRequest
+{
+    [JsonProperty("client_id")]
+    public string ClientId { get; set; }
+
+    [JsonProperty("client_secret")]
+    public string ClientSecret { get; set; }
+
+    [JsonProperty("audience")]
+    public Uri Audience { get; set; }
+
+    [JsonProperty("grant_type")]
+    public string GrantType { get; set; }
+}
---
+++ Trackly.API/Models/Auth/AuthUserIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace TracklyApi.Models.Auth;
+public class AuthUserIdentity
+{
+    [JsonProperty("connection")]
+    public string Connection { get; set; }
+
+    [JsonProperty("user_id")]
+    public string UserId { get; set; }
+
+    [JsonProperty("provider")]
+    public string Provider { get; set; }
+
+    [JsonProperty("isSocial")]
+    public bool IsSocial { get; set; }
+}
