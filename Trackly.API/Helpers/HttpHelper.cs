45f69cf0ad516d4bfc157658b4b3d673693b499c
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-20 01:54:28 +0300

    Remove unnecessary using statements in HttpHelper.cs

--- Trackly.API/Helpers/HttpHelper.cs
+++ Trackly.API/Helpers/HttpHelper.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
 using Newtonsoft.Json;
 
 namespace TracklyApi.Helpers;
