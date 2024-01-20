5db72db93a08193e7d94adf8831ca2845a09b449
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-20 23:48:20 +0300

    Add TabPage with bottom navigation bar

---
+++ Trackly.APP/trackly_app/lib/src/pages/tab_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:trackly_app/src/pages/home_page.dart';
+import 'package:trackly_app/src/pages/auth/profile_page.dart';
+import 'package:trackly_app/src/pages/scan_page.dart';
+import 'package:trackly_app/src/pages/tickets/work_item_page.dart';
+import 'package:trackly_app/src/utils/all_constants_imports.dart';
+import 'package:trackly_app/src/utils/app_resources.dart';
+
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
+class TabPage extends StatefulWidget {
+  const TabPage({Key? key}) : super(key: key);
+
+  @override
+  State<TabPage> createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+            //dispose the previous page when the index changes
+            if (index == 0) {
+              _pageController.dispose();
+            }
+          },
+          children: <Widget>[
+            HomePage(),
+            WorkItemPage(),
+            ScanPage(),
+            HomePage(),
+            ProfilePage()
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        showElevation: true,
+        itemCornerRadius: 24,
+        curve: Curves.easeIn,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: [
+          BottomNavyBarItem(
+            icon: Icon(Icons.apps),
+            title: Text('Home'),
+            activeColor: Colors.red,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.people),
+            title: Text('Users'),
+            activeColor: Colors.purpleAccent,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: const ImageIcon(
+              AssetImage(AppAssets.scanIcon),
+            ),
+            title: Text(
+              'Scan',
+            ),
+            activeColor: Colors.pink,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Settings'),
+            activeColor: Colors.blue,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Settings'),
+            activeColor: Colors.blue,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
