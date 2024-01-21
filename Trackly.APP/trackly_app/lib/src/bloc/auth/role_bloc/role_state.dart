bde66cc5433e2422c1d0994e1ee6400003d03238
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-21 20:34:59 +0300

    Add role loading states and failure state to RoleState

--- Trackly.APP/trackly_app/lib/src/bloc/auth/role_bloc/role_state.dart
+++ Trackly.APP/trackly_app/lib/src/bloc/auth/role_bloc/role_state.dart
@@ -11,6 +11,21 @@ abstract class RoleState extends Equatable {
 
 class RoleInitial extends RoleState {}
 
+class AddingRoleLoading extends RoleState {}
+
+class RemovingRoleLoading extends RoleState {}
+
+class CheckingRoleLoading extends RoleState {}
+
 class RoleAdmin extends RoleState {}
 
 class RoleTechnician extends RoleState {}
+
+class RoleFailure extends RoleState {
+  final String message;
+
+  const RoleFailure({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
