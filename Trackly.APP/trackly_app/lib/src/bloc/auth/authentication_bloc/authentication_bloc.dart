9e70f85c15b11d90ec37821f86622bb5cb4fe718
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-21 20:34:31 +0300

    Add role state change and update event names

--- Trackly.APP/trackly_app/lib/src/bloc/auth/authentication_bloc/authentication_bloc.dart
+++ Trackly.APP/trackly_app/lib/src/bloc/auth/authentication_bloc/authentication_bloc.dart
@@ -1,19 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:trackly_app/src/bloc/auth/role_bloc/role_bloc.dart';
+import 'package:trackly_app/src/bloc/auth/role_bloc/role_event.dart';
 import 'package:trackly_app/src/data/localstorage/shared_reference_manager.dart';
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(AuthenticationUnitialized()) {
+  // Singleton instance
+  static final AuthenticationBloc _instance = AuthenticationBloc._internal();
+
+  factory AuthenticationBloc() {
+    return _instance;
+  }
+  AuthenticationBloc._internal() : super(AuthenticationUnitialized()) {
     on<AppStarted>((event, emit) async {
       emit(AuthenticationLoading());
       try {
         final isLoggedIn = await SharedPreferencesManager().isLoggedIn();
 
         if (isLoggedIn) {
+          //change role state to admin
+          RoleBloc().add(CheckRoleEvent());
           emit(AuthenticationAuthenticated());
         } else {
           emit(AuthenticationUnauthenticated());
@@ -23,13 +33,13 @@ class AuthenticationBloc
       }
     });
 
-    on<LoginEvent>((event, emit) async {
+    on<LoginAuthenticationEvent>((event, emit) async {
       emit(AuthenticationLoading());
       Future.delayed(const Duration(seconds: 1));
       emit(AuthenticationAuthenticated());
     });
 
-    on<LogoutEvent>((event, emit) async {
+    on<LogoutAuthenticationEvent>((event, emit) async {
       emit(AuthenticationLoading());
       Future.delayed(const Duration(seconds: 1));
       emit(AuthenticationUnauthenticated());
