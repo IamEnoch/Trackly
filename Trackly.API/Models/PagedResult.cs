205261db7c0293e2492ff7f7f609ac4ae28637dc
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-21 23:21:45 +0300

    Add PagedResult class to Trackly.API.Models

---
+++ Trackly.API/Models/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace TracklyApi.Models
+{
+    public class PagedResult<T>
+    {
+        public int PageNumber { get; set; }
+        public int TotalCount { get; set; }
+        public int RecordNumber { get; set; }
+        public IList<T> Items { get; set; }
+    }
+}
