52c12ff59108898df39c366ec31b443572ff4bf1
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-26 18:09:41 +0300

    Remove commented out code in scan_page.dart

--- Trackly.APP/trackly_app/lib/src/pages/scan_page.dart
+++ Trackly.APP/trackly_app/lib/src/pages/scan_page.dart
@@ -19,40 +19,6 @@ class ScanPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mobile Scanner'),
-        // actions: [
-        //   IconButton(
-        //     color: Colors.white,
-        //     icon: ValueListenableBuilder(
-        //       valueListenable: cameraController.torchState,
-        //       builder: (context, state, child) {
-        //         switch (state as TorchState) {
-        //           case TorchState.off:
-        //             return const Icon(Icons.flash_off, color: Colors.grey);
-        //           case TorchState.on:
-        //             return const Icon(Icons.flash_on, color: Colors.yellow);
-        //         }
-        //       },
-        //     ),
-        //     iconSize: 32.0,
-        //     onPressed: () => cameraController.toggleTorch(),
-        //   ),
-        //   IconButton(
-        //     color: Colors.grey,
-        //     icon: ValueListenableBuilder(
-        //       valueListenable: cameraController.cameraFacingState,
-        //       builder: (context, state, child) {
-        //         switch (state as CameraFacing) {
-        //           case CameraFacing.front:
-        //             return const Icon(Icons.camera_front);
-        //           case CameraFacing.back:
-        //             return const Icon(Icons.camera_rear);
-        //         }
-        //       },
-        //     ),
-        //     iconSize: 32.0,
-        //     onPressed: () => cameraController.switchCamera(),
-        //   ),
-        // ],
       ),
       body: SafeArea(
         child: BlocListener<AssetCubit, AssetState>(
