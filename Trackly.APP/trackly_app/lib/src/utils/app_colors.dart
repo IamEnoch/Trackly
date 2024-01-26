964ed0b980a99f6a49d74dd486407e8027c2e842
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-01-26 18:09:10 +0300

    Add AppColors class with predefined color constants

---
+++ Trackly.APP/trackly_app/lib/src/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blueColor = Color(0xFF1573FE);
+  static const lightBlueColor = Color(0xFF3498DB);
+  static const pinkColor = Color(0xFFDA3365);
+  static const greyColor = Color(0xFF3D3D3D);
+  static const lightGreyColor = Color(0xFF88889D);
+  static const greenColor = Color(0xFF27AE60);
+  static const yellowColor = Color(0xFFF1C40F);
+  static const purpleColor = Color(0xFF9B59B6);
+}
