bac5e9bc32120f904fd6f72ddd52f91009cb2881
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-02-02 14:55:44 +0300

    Add User model for authentication

---
+++ Trackly.APP/trackly_app/lib/src/data/models/Auth/user.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  final String email;
+  final String username;
+  final String userId;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  User({
+    required this.email,
+    required this.username,
+    required this.userId,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        email: json["email"],
+        username: json["username"],
+        userId: json["userId"],
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "username": username,
+        "userId": userId,
+        "createdAt": createdAt.toIso8601String(),
+        "updatedAt": updatedAt.toIso8601String(),
+      };
+}
