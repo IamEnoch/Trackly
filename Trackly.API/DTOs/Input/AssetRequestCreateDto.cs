10020e8e4334c91068d57553c502448b6adeac19
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-02-11 21:59:02 +0300

    Remove AssetRequestDto class

---
+++ Trackly.API/DTOs/Input/AssetRequestCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using static TracklyApi.Helpers.EnumHelper;
+
+namespace TracklyApi.DTOs.Input
+{
+    public class AssetRequestCreateDto
+    {
+        //Properties to match the asset model
+        public string BarcodeNumber { get; set; }
+        public string AssetName { get; set; }
+        public string Category { get; set; }
+        public string DepartmentName { get; set; }
+        public string LocationName { get; set; }
+        public string SerialNumber { get; set; }
+        public string? Description { get; set; }
+        public string Condition { get; set; }
+        public string? Ram { get; set; }
+        public string AssignedTo { get; set; }
+        public string? Processor { get; set; }
+        public string? Storage { get; set; }
+        public decimal PurchaseCost { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
