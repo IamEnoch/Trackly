ca1d82fb76a1a19c69109cfc9a5d05fa0bea2054
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-02-11 21:57:04 +0300

    Add asset creation and update functionality

--- Trackly.APP/trackly_app/lib/src/bloc/app_functionality/assets/assets_cubit.dart
+++ Trackly.APP/trackly_app/lib/src/bloc/app_functionality/assets/assets_cubit.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trackly_app/Logging/logger.dart';
 import 'package:trackly_app/src/bloc/app_functionality/assets/assets_state.dart';
 import 'package:trackly_app/src/data/models/Assets/asset.dart';
+import 'package:trackly_app/src/data/models/Assets/asset_create.dart';
 import 'package:trackly_app/src/data/models/Assets/asset_update.dart';
 import 'package:trackly_app/src/data/models/api_response.dart';
 import 'package:trackly_app/src/data/services/providers/asset_provider.dart';
@@ -18,48 +19,10 @@ class AssetCubit extends Cubit<AssetState> {
   AssetCubit._internal() : super(AssetsInitial());
   final log = logger(AssetCubit);
 
-  //update asset
-  Future<void> updateAsset(String assetId, AssetUpdate asset) async {
-    emit(AssetLoading());
-    try {
-      var response = await AssetProvider().updateAsset(assetId, asset);
-      if (response.error) {
-        log.e('The asset was not updated');
-        emit(AssetFailure(message: response.errorMessage));
-        log.e(response.errorMessage);
-        return;
-      } else {
-        log.d('The asset was updated');
-        emit(AssetUpdated());
-      }
-    } on Exception catch (e) {
-      emit(AssetFailure(message: e.toString()));
-    }
-  }
-
-  Future<void> createWorkItem(Asset asset) async {
-    emit(AssetLoading());
-    try {
-      var response = await AssetProvider().createAsset(asset);
-      if (response.error) {
-        log.e('The work item was not created');
-        emit(AssetFailure(message: response.errorMessage));
-        log.e(response.errorMessage);
-        return;
-      } else {
-        log.d('The work item was created');
-        emit(AssetCreated());
-      }
-    } on Exception catch (e) {
-      emit(AssetFailure(message: e.toString()));
-    }
-  }
-
+  //Get paginated assets
   Future<List<Asset>> getAssets(int currentPage, int pageSize) async {
     try {
       emit(AssetsLoading());
-
-      // Api call
       ApiResponse<List<Asset>?> assets =
           await AssetProvider().getPaginatedAssets(currentPage, pageSize);
 
@@ -79,13 +42,8 @@ class AssetCubit extends Cubit<AssetState> {
     }
   }
 
-  // @override
-  // Future<void> close() {
-  //   _assetStreamController.close();
-  //   return super.close();
-  // }
-
-  Future<void> fetchAsset(String id) async {
+  //Get an asset
+  Future<void> getAsset(String id) async {
     emit(AssetLoading());
     try {
       final response = await AssetProvider().getAssetByBarcodeNumber(id);
@@ -101,6 +59,44 @@ class AssetCubit extends Cubit<AssetState> {
       } else {
         emit(AssetFetched(asset: response.response!));
         log.d('The response is ${response.response}');
+      }
+    } on Exception catch (e) {
+      emit(AssetFailure(message: e.toString()));
+    }
+  }
+
+  //create asset
+  Future<void> createAsset(AssetCreate asset) async {
+    emit(AssetLoading());
+    try {
+      var response = await AssetProvider().createAsset(asset);
+      if (response.error) {
+        log.e('The aset was not created');
+        emit(AssetFailure(message: response.errorMessage));
+        log.e(response.errorMessage);
+        return;
+      } else {
+        log.d('The asset was created');
+        emit(AssetCreated());
+      }
+    } on Exception catch (e) {
+      emit(AssetFailure(message: e.toString()));
+    }
+  }
+
+  //update asset
+  Future<void> updateAsset(String assetId, AssetUpdate asset) async {
+    emit(AssetLoading());
+    try {
+      var response = await AssetProvider().updateAsset(assetId, asset);
+      if (response.error) {
+        log.e('The asset was not updated');
+        emit(AssetFailure(message: response.errorMessage));
+        log.e(response.errorMessage);
+        return;
+      } else {
+        log.d('The asset was updated');
+        emit(AssetUpdated());
       }
     } on Exception catch (e) {
       emit(AssetFailure(message: e.toString()));
