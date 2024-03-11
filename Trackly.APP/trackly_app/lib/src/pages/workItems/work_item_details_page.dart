8c4a1f4347981788b42e5d3a05196c4027fb0cb2
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-03-11 22:03:20 +0300

    Fix bug in login functionality

---
+++ Trackly.APP/trackly_app/lib/src/pages/workItems/work_item_details_page.dart
@@ -0,0 +1,332 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:trackly_app/src/utils/app_resources.dart';
+import 'package:trackly_app/src/utils/widgets/status_card.dart';
+
+class WorkItemDetailsPage extends StatefulWidget {
+  const WorkItemDetailsPage({super.key});
+
+  @override
+  State<WorkItemDetailsPage> createState() => _WorkItemDetailsPageState();
+}
+
+class _WorkItemDetailsPageState extends State<WorkItemDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    // final arguments =
+    //     ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
+    return Scaffold(
+      backgroundColor: const Color(0xffF5F7FA),
+      body: SafeArea(
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.059),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.0409,
+                ),
+                SizedBox(
+                  width: 35,
+                  height: 35,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_sharp,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.0309,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.877,
+                  child: Card(
+                    color: const Color(0xffFFFFFF),
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24)),
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.028,
+                        bottom: MediaQuery.of(context).size.height * 0.028,
+                        left: MediaQuery.of(context).size.width * 0.0610,
+                        right: MediaQuery.of(context).size.width * 0.0610,
+                      ),
+                      child: Column(
+                        children: [
+                          // SizedBox(
+                          //   height: MediaQuery.of(context).size.height * 0.018,
+                          // ),
+                          Text(
+                            'Work Item Name',
+                            style: TextStyle(
+                              fontFamily: GoogleFonts.poppins().fontFamily,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              color: const Color(0xff474747),
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.009,
+                          ),
+                          Text(
+                            "Yes",
+                            style: TextStyle(
+                              fontFamily: GoogleFonts.poppins().fontFamily,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 24,
+                              color: const Color(0xff121212),
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.037,
+                          ),
+                          Container(
+                            color: const Color(0xffEDEDED),
+                            height: 0.5,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.037,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Description',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'Yes',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Department',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              Text(
+                                'Department',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Container(
+                            color: const Color(0xffEDEDED),
+                            height: 0.5,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Date',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              Text(
+                                'Yes',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Time',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              Text(
+                                'Yes',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Assigned to',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              Text(
+                                'Assigned to',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Branch',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              Text(
+                                'Branch',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Priority',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.poppins().fontFamily,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 13,
+                                  color: const Color(0xff707070),
+                                ),
+                              ),
+                              Text(
+                                'Yes',
+                                textAlign: TextAlign.end,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Status',
+                                textAlign: TextAlign.start,
+                                style: AppResources()
+                                    .appStyles
+                                    .textStyles
+                                    .bodyDefaultBold,
+                              ),
+                              StatusCardWidget(
+                                status: 'Yes',
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Container(
+                            color: const Color(0xffEDEDED),
+                            height: 0.5,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.0387,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
