cbc4aea20f37509143b1c2b8fd7e7992e8144857
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-03-11 23:58:40 +0300

    Add changeWorkItemStatus method to WorkItemRepository

--- Trackly.APP/trackly_app/lib/src/data/services/repositories/work_item_repository.dart
+++ Trackly.APP/trackly_app/lib/src/data/services/repositories/work_item_repository.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 import 'package:trackly_app/Logging/logger.dart';
+import 'package:trackly_app/src/data/enumhelper/enums.dart';
 import 'package:trackly_app/src/data/localstorage/shared_reference_manager.dart';
 import 'package:trackly_app/src/data/models/WorkItems/work_item_create.dart';
+import 'package:trackly_app/src/data/models/WorkItems/work_item_status_update.dart';
 import 'package:trackly_app/src/data/models/api_response.dart';
 import 'package:trackly_app/src/data/models/WorkItems/work_item.dart';
 
@@ -38,6 +40,42 @@ class WorkItemRepository {
           'Authorization': 'Bearer $authToken',
         },
         body: convert.jsonEncode(workItem.toJson()),
+      );
+
+      if (response.statusCode != 200) {
+        log.e('The status code is ${response.statusCode}');
+        return ApiResponse(
+            response: null, errorMessage: 'Bad request', error: true);
+      } else {
+        log.e('The status code is ${response.statusCode}');
+        return ApiResponse(response: null, errorMessage: 'error', error: false);
+      }
+    } catch (e) {
+      log.e(e);
+      return ApiResponse(response: null, errorMessage: 'No error', error: true);
+    }
+  }
+
+  //update the status of workItem
+  Future<ApiResponse<void>> changeWorkItemStatus(
+      String id, WorkItemStatusUpdate workItemStatusUpdate) async {
+    Response? response;
+    String authToken = await SharedPreferencesManager().getAuthAccessToken();
+
+    try {
+      //Make a POST request to the API
+      var uri = Uri.http(basicUrl, '/workitems/${id}/status');
+      log.d('The uri is ${uri.toString()}');
+      response = await http.put(
+        Uri.parse(
+          uri.toString(),
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'text/plain',
+          'Authorization': 'Bearer $authToken',
+        },
+        body: convert.jsonEncode(workItemStatusUpdate.toJson()),
       );
 
       if (response.statusCode != 200) {
