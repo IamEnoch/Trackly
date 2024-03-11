15cf7c41934dedc6a38707018f32ffedcfcd5636
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-03-11 23:58:34 +0300

    Add work item status update functionality

--- Trackly.APP/trackly_app/lib/src/data/services/providers/work_item_provider.dart
+++ Trackly.APP/trackly_app/lib/src/data/services/providers/work_item_provider.dart
@@ -1,4 +1,5 @@
 import 'package:trackly_app/src/data/models/WorkItems/work_item_create.dart';
+import 'package:trackly_app/src/data/models/WorkItems/work_item_status_update.dart';
 import 'package:trackly_app/src/data/models/api_response.dart';
 import 'package:trackly_app/src/data/models/WorkItems/work_item.dart';
 import 'package:trackly_app/src/data/services/repositories/work_item_repository.dart';
@@ -7,6 +8,12 @@ class WorkItemProvider {
   //Create work item
   Future<ApiResponse<void>> createWorkItem(WorkItemCreate workItem) {
     return WorkItemRepository().createWorkItem(workItem);
+  }
+
+  //Update work item status
+  Future<ApiResponse<void>> changeWorkItemStatus(
+      String id, WorkItemStatusUpdate workItemStatusUpdate) {
+    return WorkItemRepository().changeWorkItemStatus(id, workItemStatusUpdate);
   }
 
   //Get paginated work items
