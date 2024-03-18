f4c0a6bba5f0a2d9c8b897133783e0a23d06a0d4
Enoch Mwangi <enoch.mwangi@outlook.com>
2024-03-18 11:42:25 +0300

    Refactor TicketsController.cs

--- Trackly.API/Controllers/TicketsController.cs
+++ Trackly.API/Controllers/TicketsController.cs
@@ -26,62 +26,7 @@ namespace TracklyApi.Controllers
             _logger = logger;
             _context = context;
         }
-        //get work item by id
-        [HttpGet("workitem/{workItemId}")]
-        public async Task<ActionResult<WorkItem>> GetWorkItemById(string workItemId)
-        {
-            try
-            {
-                var workItem = await _context.WorkItems.FindAsync(Guid.Parse(workItemId));
-                if (workItem == null)
-                {
-                    return NotFound("Work item not found");
-                }
-
-                return Ok(workItem);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        [HttpPost("workitem")]
-        public async Task<ActionResult<WorkItem>> CreateWorkItem([FromBody]WorkItemRequestDto workItemRequestDto)
-        {
-            try
-            {
-                //check if asset id exists first or assetId fails to parse or assetId is null            
-                var asset = await _context.Assets.FindAsync(Guid.Parse(workItemRequestDto.AssetId));
-                if (!Guid.TryParse(workItemRequestDto.AssetId, out Guid assetId))
-                {
-                    return BadRequest("Asset Id is not valid");
-                }
-
-                var workItem = new WorkItem
-                {
-                    Title = workItemRequestDto.Title,
-                    Description = workItemRequestDto.Description,
-                    Status = EnumHelper.WorkItemStatus.Pending,
-                    Priority = Enum.Parse<EnumHelper.Priority>(workItemRequestDto.Priority),
-                    Category = Enum.Parse<EnumHelper.TicketCategory>(workItemRequestDto.Category),
-                    AssetId = assetId,
-                    CreatedAt = DateTime.Now
-                };
-
-                _context.WorkItems.Add(workItem);
-                await _context.SaveChangesAsync();
-
-
-                var resultId = new { id = workItem.WorkItemId};
-                return await GetWorkItemById(resultId.id.ToString());
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-
-        }
+        
 
         //put request to change the status of a ticket
         [HttpPut("{ticketId}/status")]
@@ -129,7 +74,7 @@ namespace TracklyApi.Controllers
         }
 
         //Get paginated tickets(TicketDto) that have been completed 
-        [HttpGet("closed")]
+        [HttpGet("completed")]
         public async Task<ActionResult<IEnumerable<TicketDto>>> GetPagedCompletedTickets(
             [FromQuery] QueryParameters parameters)
         {
