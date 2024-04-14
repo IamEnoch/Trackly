c5d3ca7631d9130363ab254f43c6166ff74caf30
Enoch Mwangi <76245976+IamEnoch@users.noreply.github.com>
2024-04-14 22:41:46 +0300

    fix

--- Trackly.API/Data/AppDbContext.cs
+++ Trackly.API/Data/AppDbContext.cs
@@ -119,11 +119,7 @@ namespace TracklyApi.Data
                     Status = (WorkItemStatus)(i % Enum.GetValues(typeof(WorkItemStatus)).Length),
                     Priority = (Priority)(i % Enum.GetValues(typeof(Priority)).Length),
                     Category = (TicketCategory)(i % Enum.GetValues(typeof(TicketCategory)).Length),
-<<<<<<< HEAD
-                    CreatorUserID = usersIds[i % 4] ,//Select any id
-=======
-                    CreatorUserId = null, // Set CreatorUserID as null for now
->>>>>>> feature/mobile-app-enhancements
+                    CreatorUserId = usersIds[i % 4] ,//Select any id
                     AssetId = assetGuids[i % 70], // Use Asset ID
                     CreatedAt = DateTime.Now.AddDays(-i)
                 });
